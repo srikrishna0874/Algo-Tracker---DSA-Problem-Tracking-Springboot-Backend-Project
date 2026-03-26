73fbc2c5b0bd482e3023ba1eddab2853c325f1c0
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2026-03-26 23:05:17 +0530

    Initial Project setup

---
+++ src/test/java/com/algotracker/AlgotrackerProject/AlgotrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algotracker.AlgotrackerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgotrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
