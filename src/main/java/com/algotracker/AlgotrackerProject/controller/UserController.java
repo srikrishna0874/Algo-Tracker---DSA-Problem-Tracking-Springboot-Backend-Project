4dad865be4e773531f5331e70c3ff22b4c4f77aa
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2026-04-08 20:45:05 +0530

    removed try-catch everywhere since we will implement global exception handler later

--- src/main/java/com/algotracker/AlgotrackerProject/controller/UserController.java
+++ src/main/java/com/algotracker/AlgotrackerProject/controller/UserController.java
@@ -27,54 +27,44 @@ public class UserController {
 
     @PostMapping
     ResponseEntity<ApiResponse<UserResponseDto>> postUser(@Valid @RequestBody UserRequestDto userRequestDto) {
-        try {
-            User user = userService.postUser(userRequestDto);
-            UserResponseDto userResponseDto = userMapper.toDto(user);
-            return ResponseEntity.status(HttpStatusCode.valueOf(201))
-                    .body(new ApiResponse<>(true, "User created successfully", userResponseDto));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, e.getMessage(), null));
-        }
+
+        User user = userService.postUser(userRequestDto);
+        UserResponseDto userResponseDto = userMapper.toDto(user);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201))
+                .body(new ApiResponse<>(true, "User created successfully", userResponseDto));
 
 
     }
 
     @GetMapping("/{userId}")
     ResponseEntity<ApiResponse<UserResponseDto>> getUser(@PathVariable int userId) {
-        try {
-            User user = userService.getUser(userId);
-            UserResponseDto userResponseDto = userMapper.toDto(user);
 
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ApiResponse<UserResponseDto>(true, "User fetched successfully", userResponseDto));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, e.getMessage(), null));
-        }
+        User user = userService.getUser(userId);
+        UserResponseDto userResponseDto = userMapper.toDto(user);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<UserResponseDto>(true, "User fetched successfully", userResponseDto));
+
 
     }
 
     @GetMapping
     ResponseEntity<ApiResponse<List<UserResponseDto>>> getAllUsers() {
-        try {
-            List<User> userList = userService.getAllUsers();
-            List<UserResponseDto> userResponseDtoList = userList.stream().map(userMapper::toDto).toList();
 
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ApiResponse<List<UserResponseDto>>(true, "Users fetched successfully",
-                            userResponseDtoList));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ApiResponse<>(false, e.getMessage(), null));
-        }
+        List<User> userList = userService.getAllUsers();
+        List<UserResponseDto> userResponseDtoList = userList.stream().map(userMapper::toDto).toList();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<List<UserResponseDto>>(true, "Users fetched successfully", userResponseDtoList));
+
     }
 
     @DeleteMapping("/{userId}")
     ResponseEntity<ApiResponse<String>> deleteUser(@PathVariable int userId) {
-        try {
-            userService.deleteUser(userId);
-            return ResponseEntity.ok(new ApiResponse<>(true, null, "User deleted successfully"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, e.getMessage(), null));
-        }
+
+        userService.deleteUser(userId);
+        return ResponseEntity.ok(new ApiResponse<>(true, null, "User deleted successfully"));
+
     }
 
 }
